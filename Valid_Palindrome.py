62347ea1cb6b241080e269d6b378d2123f010a9f
David Abushlaih <83670808+DavidA123777@users.noreply.github.com>
2024-02-18 17:34:54 -0500

    Create Valid_Palindrome.py
    solution in python for valid palindrome

---
+++ Valid_Palindrome.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        # first convert all uppercase letters to lowercase .lower()
+        # loop through and remove all non-alnum characters check .alnum(char)
+        s = s.lower()
+        newStr = ""
+        # add each letter to this new string if it is alphanumeric
+        for char in s:
+            if (char.isalnum()):
+                newStr += char
+        # create another string to store the reverse of this string
+        reversedStr = ""
+        # loop through the alphanumeric string in reverse and concatenate it to the string
+
+        for i in range(len(newStr)):
+            reversedStr += newStr[len(newStr) -1 -i]
+        # If the two strings are equivalent then return True since they are palindromes
+        # else return false because they are not equivalent forward and reverse
+        if (newStr == reversedStr):
+            return True
+        else:
+            return False
+        # Time Complexity O(n + m) = O(n) where n = len(s) and m = len(newStr)
+        # Space Complexity O(2n) = O(n) where n = newStr 
+
+
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
