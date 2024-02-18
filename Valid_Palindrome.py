20d5cf8b1af392960391fe0b8303db03dd91b813
David Abushlaih <83670808+DavidA123777@users.noreply.github.com>
2024-02-18 17:38:32 -0500

    Update Valid_Palindrome.py
    found a quicker method of solving valid palindrome without manually looping through the alphanumeric string and storing the characters in reverse in another string

--- Valid_Palindrome.py
+++ Valid_Palindrome.py
@@ -8,18 +8,26 @@ class Solution(object):
         for char in s:
             if (char.isalnum()):
                 newStr += char
-        # create another string to store the reverse of this string
-        reversedStr = ""
-        # loop through the alphanumeric string in reverse and concatenate it to the string
-
-        for i in range(len(newStr)):
-            reversedStr += newStr[len(newStr) -1 -i]
-        # If the two strings are equivalent then return True since they are palindromes
-        # else return false because they are not equivalent forward and reverse
-        if (newStr == reversedStr):
+        if (newStr == newStr[::-1]):
             return True
         else:
             return False
+                
+        # slower method
+        # # create another string to store the reverse of this string
+        # reversedStr = ""
+        # # loop through the alphanumeric string in reverse and concatenate it to the string
+
+        # for i in range(len(newStr)):
+        #     reversedStr += newStr[len(newStr) -1 -i]
+        # # If the two strings are equivalent then return True since they are palindromes
+        # # else return false because they are not equivalent forward and reverse
+        # if (newStr == reversedStr):
+        #     return True
+        # else:
+        #     return False
+
+        # 
         # Time Complexity O(n + m) = O(n) where n = len(s) and m = len(newStr)
         # Space Complexity O(2n) = O(n) where n = newStr 
 
